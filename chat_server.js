e2cd66c8ed17241fc25e86aa251796dc532e68b5
XianNg24 <49993481+XianNg24@users.noreply.github.com>
2022-05-05 23:10:01 +0800

    first commit

---
+++ chat_server.js
@@ -0,0 +1,228 @@
+const express = require("express");
+
+const bcrypt = require("bcrypt");
+const fs = require("fs");
+const session = require("express-session");
+
+// Create the Express app
+const app = express();
+
+// Use the 'public' folder to serve static files
+app.use(express.static("public"));
+
+// Use the json middleware to parse JSON data
+app.use(express.json());
+
+// Use the session middleware to maintain sessions
+const chatSession = session({
+    secret: "game",
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: { maxAge: 300000 }
+});
+app.use(chatSession);
+
+// This helper function checks whether the text only contains word characters
+function containWordCharsOnly(text) {
+    return /^\w+$/.test(text);
+}
+
+// Handle the /register endpoint
+app.post("/register", (req, res) => {
+    // Get the JSON data from the body
+    const { username, avatar, name, password } = req.body;
+
+    //
+    // D. Reading the users.json file
+    //
+    const jsonData = fs.readFileSync("data/users.json");
+    const users = JSON.parse(jsonData);
+    
+    //
+    // E. Checking for the user data correctness
+    //
+    if (!username || !avatar || !name || !password){
+        res.json({ status: "error",
+                    error: "Username/avatar/name/password cannot be empty." });
+        return;
+    }
+    if (!(/^\w+$/i.test(username))){
+        res.json({ status: "error",
+                    error: "Username is not valid." });
+        return;
+    }
+    if (users[username]){
+        res.json({ status: "error",
+                    error: "Username is already taken." });
+        return;
+    }
+    //
+    // G. Adding the new user account
+    //
+    const hash = bcrypt.hashSync(password, 10);
+    users[username] = {avatar, name, password: hash};
+    
+    //
+    // H. Saving the users.json file
+    //
+    fs.writeFileSync("data/users.json", JSON.stringify(users, null, " "));
+    
+    //
+    // I. Sending a success response to the browser
+    //
+    res.json({ status:"success" });
+
+    // Delete when appropriate
+    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
+});
+
+// Handle the /signin endpoint
+app.post("/signin", (req, res) => {
+    // Get the JSON data from the body
+    const { username, password } = req.body;
+
+    //
+    // D. Reading the users.json file
+    //
+    const jsonData = fs.readFileSync("data/users.json");
+    const users = JSON.parse(jsonData);
+
+    //
+    // E. Checking for username/password
+    //
+    if (!users[username]){
+        res.json({ status: "error",
+                    error: "Username is not found." });
+        return;
+    }
+    const hashedPassword = users[username].password;
+    if (!bcrypt.compareSync(password, hashedPassword)) {
+        res.json({ status: "error",
+                    error: "Wrong password." });
+        return;
+    }
+
+    //
+    // G. Sending a success response with the user account
+    //
+    req.session.user = {username, avatar:users[username].avatar, name:users[username].name};
+    res.json({ status: "success", user: req.session.user });
+
+    // Delete when appropriate
+    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
+});
+
+// Handle the /validate endpoint
+app.get("/validate", (req, res) => {
+
+    //
+    // B. Getting req.session.user
+    //
+    if(!req.session.user){
+        res.json({status: "error", error: "You have not signed in."});
+        return;
+    }
+    //
+    // D. Sending a success response with the user account
+    //
+    res.json({ status: "success", user: req.session.user });
+ 
+    // Delete when appropriate
+    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
+});
+
+// Handle the /signout endpoint
+app.get("/signout", (req, res) => {
+
+    //
+    // Deleting req.session.user
+    //
+    if(req.session.user){
+        delete req.session.user;
+    }
+    //
+    // Sending a success response
+    //
+    res.json({ status: "success"});
+
+    // Delete when appropriate
+    // res.json({ status: "error", error: "This endpoint is not yet implemented." });
+});
+
+
+//
+// ***** Please insert your Lab 6 code here *****
+//
+
+
+// Use a web server to listen at port 8000
+
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer( app );
+const io = new Server(httpServer);
+
+httpServer.listen(8000, () => {
+    console.log("The chat server has started...");
+});
+
+io.use((socket, next) => {
+    chatSession(socket.request, {}, next);
+});
+
+const onlineUsers = {}
+
+io.on("connection", (socket) => {
+    if(socket.request.session.user){
+        const { username, avatar, name} = socket.request.session.user;
+        onlineUsers[username] = { avatar, name };
+        console.log(onlineUsers);
+        io.emit("add user", JSON.stringify(socket.request.session.user));
+    }
+
+    socket.on("disconnect", () => {
+        if(socket.request.session.user){
+            const { username } = socket.request.session.user;
+            if (onlineUsers[username]) delete onlineUsers[username];
+            console.log(onlineUsers);
+            io.emit("remove user", JSON.stringify(socket.request.session.user));
+        }
+    });
+
+    socket.on("get users", () => {
+        socket.emit("users", JSON.stringify(onlineUsers));
+    });
+
+    socket.on("get messages", () => {
+        const chatroom = JSON.parse(fs.readFileSync("data/chatroom.json"));
+        socket.emit("messages", JSON.stringify(chatroom));
+    });
+
+    socket.on("post message", (content) => {
+        if(socket.request.session.user){
+            const message = {
+                user: socket.request.session.user,
+                datetime: new Date(),
+                content: content
+            };
+            const chatroom = JSON.parse(fs.readFileSync("data/chatroom.json"));
+            chatroom.push(message);
+            fs.writeFileSync("data/chatroom.json", JSON.stringify(chatroom, null, " "));
+            io.emit("add message", JSON.stringify(message));
+        }
+    });
+
+    socket.on("typing", () => {
+        if(socket.request.session.user){
+            const typingMessage = String(socket.request.session.user.name) + " is typing...";
+            const message = {
+                user: socket.request.session.user,
+                typingMessage: typingMessage
+            };
+            io.emit("add typing", JSON.stringify(message));
+        }
+    });
+});
+
+
