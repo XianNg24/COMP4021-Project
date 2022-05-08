c71345b6ce8034f4da02e531022cf22d7fd61cb9
Tony Tong <ztangaj@connect.ust.hk>
2022-05-08 18:47:00 +0800

    change sprite

--- public/player.js
+++ public/player.js
@@ -10,26 +10,22 @@ const Player = function(ctx, x, y, gameArea) {
     // and the moving sprite sequences `moveLeft`, `moveUp`, `moveRight` and `moveDown`.
     const sequences = {
         /* Idling sprite sequences for facing different directions */
-        idleLeft:  { x: 0, y: 25, width: 24, height: 25, count: 3, timing: 2000, loop: false },
-        idleUp:    { x: 0, y: 50, width: 24, height: 25, count: 1, timing: 2000, loop: false },
-        idleRight: { x: 0, y: 75, width: 24, height: 25, count: 3, timing: 2000, loop: false },
-        idleDown:  { x: 0, y:  0, width: 24, height: 25, count: 3, timing: 2000, loop: false },
+        idleLeft:  { x: 840, y: 0, width: 56, height: 44, flip: 1, count: 6, timing: 100, loop: true },
+        idleRight: { x: 0, y: 0, width: 56, height: 44, flip: 0, count: 6, timing: 100, loop: true },
 
         /* Moving sprite sequences for facing different directions */
-        moveLeft:  { x: 0, y: 125, width: 24, height: 25, count: 10, timing: 50, loop: true },
-        moveUp:    { x: 0, y: 150, width: 24, height: 25, count: 10, timing: 50, loop: true },
-        moveRight: { x: 0, y: 175, width: 24, height: 25, count: 10, timing: 50, loop: true },
-        moveDown:  { x: 0, y: 100, width: 24, height: 25, count: 10, timing: 50, loop: true }
+        moveLeft:  { x: 840, y: 112, width: 56, height: 44, flip: 1, count: 8, timing: 100, loop: true },
+        moveRight: { x: 0, y: 112, width: 56, height: 44, flip: 0, count: 8, timing: 100, loop: true }
     };
 
     // This is the sprite object of the player created from the Sprite module.
     const sprite = Sprite(ctx, x, y);
 
     // The sprite object is configured for the player sprite here.
-    sprite.setSequence(sequences.idleDown)
+    sprite.setSequence(sequences.idleLeft)
           .setScale(2)
           .setShadowScale({ x: 0.75, y: 0.20 })
-          .useSheet("player_sprite.png");
+          .useSheet("long_char_blue_1.png");
 
     // This is the moving direction, which can be a number from 0 to 4:
     // - `0` - not moving
@@ -39,6 +35,11 @@ const Player = function(ctx, x, y, gameArea) {
     // - `4` - moving down
     let direction = 0;
 
+    // This record the direction of character, which can be either 0 or 1:
+    // - `0` - face to left
+    // - `1` - face to right
+    let face = 0;
+
     // This is the moving speed (pixels per second) of the player
     let speed = 150;
 
@@ -46,11 +47,15 @@ const Player = function(ctx, x, y, gameArea) {
     // - `dir` - the moving direction (1: Left, 2: Up, 3: Right, 4: Down)
     const move = function(dir) {
         if (dir >= 1 && dir <= 4 && dir != direction) {
+            // let direct = dir
+            // if (direct == 2 || direct == 4) direct = direction
             switch (dir) {
-                case 1: sprite.setSequence(sequences.moveLeft); break;
-                case 2: sprite.setSequence(sequences.moveUp); break;
-                case 3: sprite.setSequence(sequences.moveRight); break;
-                case 4: sprite.setSequence(sequences.moveDown); break;
+                case 1: face = 0; break;
+                case 3: face = 1; break;
+            }
+            switch (face) {
+                case 0: sprite.setSequence(sequences.moveLeft); break;
+                case 1: sprite.setSequence(sequences.moveRight); break;
             }
             direction = dir;
         }
@@ -60,11 +65,9 @@ const Player = function(ctx, x, y, gameArea) {
     // - `dir` - the moving direction when the player is stopped (1: Left, 2: Up, 3: Right, 4: Down)
     const stop = function(dir) {
         if (direction == dir) {
-            switch (dir) {
-                case 1: sprite.setSequence(sequences.idleLeft); break;
-                case 2: sprite.setSequence(sequences.idleUp); break;
-                case 3: sprite.setSequence(sequences.idleRight); break;
-                case 4: sprite.setSequence(sequences.idleDown); break;
+            switch (face) {
+                case 0: sprite.setSequence(sequences.idleLeft); break;
+                case 1: sprite.setSequence(sequences.idleRight); break;
             }
             direction = 0;
         }
